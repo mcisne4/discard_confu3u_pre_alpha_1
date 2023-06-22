1ddb4b060e72c2d7f37d54f0a45d6bdd860d6add
mcisne4 <mcisne4@gmail.com>
2023-06-22 12:48:45 -0500

    rs_logs: Refactoring

--- rs_logs_macro/src_reference/lib.rs
+++ rs_logs_macro/src_reference/lib.rs
@@ -1,10 +1,12 @@
-mod logger_derive;
-use logger_derive::logger_derive;
+mod macro_logger;
 
-#[proc_macro_derive(Logger, attributes(config, info_msg, warn_msg, error_msg))]
+#[proc_macro_derive(
+    Logger,
+    attributes(config, crate_idx, mod_idx, location, info_msg, warn_msg, error_msg)
+)]
 pub fn logger_derive_macro(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    match logger_derive(item) {
-        Ok(tokens) => tokens.into(),
+    match macro_logger::macro_logger(item) {
+        Ok(token_stream) => token_stream.into(),
         Err(e) => e.to_compile_error().into(),
     }
 }
